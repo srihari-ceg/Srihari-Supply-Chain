7528bf373072c8a02dc8d2de588189152917c1ac
Srihari <srihari.ceg@gmail.com>
2022-07-18 00:24:47 +0530

    srihari angular routing

---
+++ supply-chain/src/app/Login/main-login-page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl:'./main-login-page.html'
+    
+})
+export class loginComponent{
+   
+      }
--- supply-chain/src/app/app.module.ts
+++ supply-chain/src/app/app.module.ts
@@ -3,18 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { loginComponent } from './Login/main-login-page';
 import { productComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    productComponent
+    productComponent,
+    loginComponent,
+    ProductDetailComponent
+
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([ 
+      {path:'welcome',component:loginComponent},
+    {path:'product',component:productComponent},
+    {path:'',redirectTo:'welcome',pathMatch:'full'},
+    {path:'',redirectTo:'welcome',pathMatch:'full'}
+    ])
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
